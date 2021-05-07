83b99d52af2f45ac0843c6a11a907a46940f3822
Joaquinzer <joaquin@DESKTOP-0MAU4T7.localdomain>
2021-05-07 12:46:53 -0500

    convolution con padding

--- Convolution.py
+++ Convolution.py
@@ -1,23 +1,7 @@
-"""
-By Abhisek Jana
-code taken from https://github.com/adeveloperdiary/blog/tree/master/Computer_Vision/Sobel_Edge_Detection
-blog http://www.adeveloperdiary.com/data-science/computer-vision/how-to-implement-sobel-edge-detection-using-python-from-scratch/
-Modified by Benjamin Valdes and Joaquin Zermeno
-"""
 
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
-#esta funcion se usa para multiplicar los fragmentos por el kernel
-def conv_helper(fragment, kernel):
-    f_row, f_col = fragment.shape
-    k_row, k_col = kernel.shape 
-    result = 0.0
-    for row in range(f_row):
-        for col in range(f_col):
-            result += fragment[row,col] *  kernel[row,col]
-    return result
 
 def convolution(image, kernel):
 #aqui se hace la convolucion sin padding usando el alto y ancho de la imagen
@@ -27,10 +11,17 @@ def convolution(image, kernel):
 #se crea una matriz de ceros usando numpy  
     output = np.zeros(image.shape)
 
+# se obtienen los valores del padding para que sean los mismos a los de la imagen
+    pad_height = int((kernel_row - 1) / 2)
+    pad_width = int((kernel_col - 1) / 2)
+
+# se crea una matriz usando el padding 
+    padded_image = np.zeros((image_row + (pad_height*2), image_col + (pad_width*2)))
+    padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
+
 #se itera la matriz de la imagen  
     for row in range(image_row):
         for col in range(image_col):
-                output[row, col] = conv_helper(
-                                    image[row:row + kernel_row, 
-                                    col:col + kernel_col],kernel)
+            output[row, col] = np.sum(kernel * padded_image[row: row + kernel_row, col: col + kernel_col])
+
     return output
